8789130d64cdfa9313ee42bcec3754f9c4fc220e
SayedSadat <Shafiq_Sadat@hotmail.com>
2017-02-02 05:02:40 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y python-software-properties software-properties-common
+sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install -y tmux=2.0-1~ppa1~t
 sudo apt-get -y install libreadline6 libreadline-dev libreadline-dev libreadline6-dev libconfig-dev libssl-dev tmux lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties gcc-6
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
